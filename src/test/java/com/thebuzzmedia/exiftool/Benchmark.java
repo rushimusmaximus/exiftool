df821e11b76e9b9a7083341e4874c384d87ecdfa
raisercostin <raisercostin@gmail.com>
2014-07-25 23:10:13 +0300

    formatted

--- src/test/java/com/thebuzzmedia/exiftool/Benchmark.java
+++ src/test/java/com/thebuzzmedia/exiftool/Benchmark.java
@@ -1,28 +1,31 @@
 package com.thebuzzmedia.exiftool;
 
-import com.thebuzzmedia.exiftool.ExifTool.Feature;
-import com.thebuzzmedia.exiftool.ExifTool.Tag;
-import org.apache.log4j.Logger;
-
 import java.io.File;
 import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.thebuzzmedia.exiftool.ExifTool.Feature;
+import com.thebuzzmedia.exiftool.ExifTool.Tag;
 
 public class Benchmark {
 
 	public static final int ITERATIONS = 25;
-  private static Logger log = Logger.getLogger(Benchmark.class);
+	private static Logger log = LoggerFactory.getLogger(Benchmark.class);
 
+	public static void main(String[] args) throws IOException,
+			InterruptedException {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+		// System.setProperty(ExifTool.ENV_EXIF_TOOL_PATH,
+		// "D:\\Tools\\exiftool.exe");
 
-    //System.setProperty(ExifTool.ENV_EXIF_TOOL_PATH, "D:\\Tools\\exiftool.exe");
+		final Tag[] tags = Tag.values();
+		final File[] images = new File("src/test/resources").listFiles();
 
-    final Tag[] tags = Tag.values();
-    final File[] images = new File("src/test/resources").listFiles();
-
-		log.info("Benchmark [tags=" + tags.length + ", images="
-            + images.length + ", iterations=" + ITERATIONS + "]");
-    log.info("\t" + (images.length * ITERATIONS)
+		log.info("Benchmark [tags=" + tags.length + ", images=" + images.length
+				+ ", iterations=" + ITERATIONS + "]");
+		log.info("\t" + (images.length * ITERATIONS)
 				+ " ExifTool process calls, "
 				+ (tags.length * images.length * ITERATIONS)
 				+ " total operations.\n");
@@ -36,13 +39,13 @@ public class Benchmark {
 		log.info("\t[-stay_open False]");
 		long elapsedTime = 0;
 
-		for (int i = 1; i <= ITERATIONS; i++){
-      log.info(String.format("iteration %s of %s", i, ITERATIONS));
+		for (int i = 1; i <= ITERATIONS; i++) {
+			log.info(String.format("iteration %s of %s", i, ITERATIONS));
 			elapsedTime += run(tool, images, tags);
-    }
+		}
 
 		log.info("\t\tElapsed Time: " + elapsedTime + " ms ("
-            + ((double) elapsedTime / 1000) + " secs)");
+				+ ((double) elapsedTime / 1000) + " secs)");
 		/*
 		 * -stay_open True
 		 */
@@ -50,13 +53,13 @@ public class Benchmark {
 		long elapsedTimeSO = 0;
 
 		for (int i = 1; i <= ITERATIONS; i++) {
-      log.info(String.format("iteration %s of %s", i, ITERATIONS));
+			log.info(String.format("iteration %s of %s", i, ITERATIONS));
 			elapsedTimeSO += run(toolSO, images, tags);
 		}
 
 		log.info("\t\tElapsed Time: " + elapsedTimeSO + " ms ("
-            + ((double) elapsedTimeSO / 1000) + " secs - "
-            + ((float) elapsedTime / (float) elapsedTimeSO) + "x faster)");
+				+ ((double) elapsedTimeSO / 1000) + " secs - "
+				+ ((float) elapsedTime / (float) elapsedTimeSO) + "x faster)");
 
 		// Shut down the running exiftool proc.
 		toolSO.close();
@@ -66,9 +69,9 @@ public class Benchmark {
 			throws IllegalArgumentException, SecurityException, IOException {
 		long startTime = System.currentTimeMillis();
 
-    for (File image : images){
-      tool.getImageMeta(image, tags);
-    }
+		for (File image : images) {
+			tool.getImageMeta(image, tags);
+		}
 
 		return (System.currentTimeMillis() - startTime);
 	}
